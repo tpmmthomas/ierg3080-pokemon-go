e40e43f17cd92d61c17c1b9b109b7e2f61f3d0f2
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-12-20 10:00:05 +0800

    Create battlegym.cs

---
+++ battlegym.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class BattleGym{
+    private Pokemon PlayerPokemon;  
+    private Pokemon OpponentPokemon; //computer
+    private int CurrentTurn; //1: human 2: copmuter
+    private 
+}
