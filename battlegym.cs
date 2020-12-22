70b9daefe31a76b8446b4a0405a95daca94da8fc
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-12-22 22:51:28 +0800

    update

--- battlegym.cs
+++ battlegym.cs
@@ -1,10 +1,52 @@
 using System;
 using System.Collections.Generic;
 
+public delegate void WinMethod();
+public delegate void LoseMethod();
+
 public class BattleGym{
     private Pokemon PlayerPokemon;  
+    public Pokemon GetPlayerPokemon{
+        get{return PlayerPokemon;}
+    }
     private Pokemon OpponentPokemon; //computer
+    public Pokemon GetOpponentPokemon{
+        get{return OpponentPokemon;}
+    }
     private int CurrentTurn; //1: human 2: copmuter
+    public int GetCurrentTurn{
+        get{return CurrentTurn;}
+    }
     private float PlayerCriticalRate;
     private float OpponentCriticalRate;
+    private WinMethod win;
+    private LoseMethod lose;
+
+    public BattleGym(Pokemon x, WinMethod a, LoseMethod b){
+        PlayerPokemon = x;
+        win = a;
+        lose = b;
+        CurrentTurn = 1;
+        if(PlayerPokemon.GetCP > 2000){
+            PlayerCriticalRate = 0.3;
+        }
+        else if (PlayerPokemon.GetCP > 1000){
+            PlayerCriticalRate = 0.2;
+        }
+        else{
+            PlayerCriticalRate = 0.1;
+        }
+        if(OpponentPokemon.GetCP > 2000){
+            OpponentCriticalRate = 0.3;
+        }
+        else if (OpponentPokemon.GetCP > 1000){
+            OpponentCriticalRate = 0.2;
+        }
+        else{
+            OpponentCriticalRate = 0.1;
+        }
+    }
+    public void PlayerMove(string mv){ //pass in the move name. It is unique.
+        //to be implemented. 
+    }
 }
