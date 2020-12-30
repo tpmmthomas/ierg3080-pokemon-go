5491a8ad0773191918c526df8c763d5585d8bca6
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2020-12-30 16:43:16 +0800

    Update pokemon.cs

--- PokemonGo/WpfApp2/pokemon.cs
+++ PokemonGo/WpfApp2/pokemon.cs
@@ -237,7 +237,7 @@ namespace PokemonGo
             name = x.Name;
             typeName = x.Name;
             moveslist = new AttackMoves[3];
-            Array.Copy(moveslist, x.AtkMovesList,3);
+            Array.Copy(x.AtkMovesList,moveslist,3);
             weight = rand.Next((int)(x.LowerWeight * 10), (int)(x.UpperWeight * 10 + 1)) / (float)10;
             height = rand.Next((int)(x.LowerHeight * 10), (int)(x.UpperHeight * 10 + 1)) / (float)10;
             CP = x.InitialCP;
